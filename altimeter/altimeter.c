3bb4ae0f23d3da58b72a5b57578c04869f5200cf
SPak12 <seantwelve3@gmail.com>
2019-05-20 11:46:15 +0100

    First successful compilation

--- altimeter/altimeter.c
+++ altimeter/altimeter.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <math.h>
-#include <wiringPi.h>
+#include <wiringPiI2C.h>
 #include "altimeter.h"
 
 // Calibration parameters
@@ -11,10 +11,12 @@ unsigned short ac4, ac5, ac6;
 short b1, b2, mb, mc, md;
 
 // Over-Sampling Setting (0 to 4)
-const unsigned int OSS = 1;
+const int OSS = 1;
+
+int bmp180;
 
 // Read calibrate data from the EEPROM of the BMP180
-void calibrate(int bmp180) {
+void calibrate() {
 	cal.ac1 = wiringPiI2CReadReg16(bmp180, AC1_REG);
 	cal.ac2 = wiringPiI2CReadReg16(bmp180, AC2_REG);
 	cal.ac3 = wiringPiI2CReadReg16(bmp180, AC3_REG);
@@ -53,9 +55,9 @@ unsigned int readRawPressure() {
 			usleep(25500);
 			break;
 	}
-	uint8 byte1 = wiringPiI2CReadReg8(bmp180, 0xF6);
-	uint8 byte2 = wiringPiI2CReadReg8(bmp180, 0xF7);
-	uint8 byte3 = wiringPiI2CReadReg8(bmp180, 0xF8);
+	uint8_t byte1 = wiringPiI2CReadReg8(bmp180, 0xF6);
+	uint8_t byte2 = wiringPiI2CReadReg8(bmp180, 0xF7);
+	uint8_t byte3 = wiringPiI2CReadReg8(bmp180, 0xF8);
 	unsigned int up = (((unsigned int) byte1 << 16) | ((unsigned int) byte2 << 8) | (unsigned int) byte3) >> (8 - OSS);
 	return up;
 }
@@ -66,25 +68,14 @@ int compensateTemperature() {
 	int x1 = (((int) ut - (int) cal.ac6) * (int) cal.ac5) >> 15;
 	int x2 = ((int) cal.mc << 11) / (x1 + cal.md);
 	return x1 + x2;
+}
 
 // Calculate true temperature
-int getTemperature() {
+double getTemperature() {
 	int rawTemperature = (compensateTemperature() + 8) >> 4;
-	return ((double) rawTemperature) / 10;
+	return ((double) rawTemperature) / 10.0;
 }
 
-int main(void) {
-	int bmp180 = wirintPiI2CSetup(BMP180_ADDR);
-	calibrate();
-	readRawTemperature();
-	readRawPressure();
-	double T = getTemperature();
-	double P = getPressure();
-	double a = getAltitude();
-	printf("T: %.2f\t", T);
-	printf("P: %.2f\t", P);
-	printf("a: %.2f\n", a);
-}
 // Calculate true pressure
 int getPressure() {
 	unsigned int up = readRawPressure();
@@ -92,11 +83,11 @@ int getPressure() {
 	int x1 = (cal.b2 * (b6 * b6) >> 12) >> 11;
 	int x2 = (cal.ac2 * b6) >> 11;
 	int x3 = x1 + x2;
-	int B3 = (((((int) cal.ac1) * 4 + x3) << OSS) + 2) >> 2;
+	int b3 = (((((int) cal.ac1) * 4 + x3) << OSS) + 2) >> 2;
 	x1 = (cal.ac3 * b6) >> 13;
 	x2 = (cal.b1 * ((b6 * b6) >> 12)) >> 16;
 	x3 = ((x1 + x2) + 2) >> 2;
-	unsigned int b4 = (cal.ac4 * (unsigned int) (X3 + 32768)) >> 15;
+	unsigned int b4 = (cal.ac4 * (unsigned int) (x3 + 32768)) >> 15;
 	unsigned int b7 = ((unsigned int) (up - b3) * (50000 >> OSS));
 	int p = (b7 < 0x80000000) ? (b7 << 1) / b4 : (b7 / b4) << 1;
 	x1 = (p >> 8) * (p >> 8);
@@ -113,15 +104,17 @@ double getAltitude() {
 }
 
 int main(int arc, char* argv[]) {
-	int bmp180 = wirintPiI2CSetup(BMP180_ADDR);
-	calibrate(bmp180);
-	readRawTemperature();
-	readRawPressure();
-	double T = getTemperature();
-	double P = getPressure();
-	double a = getAltitude();
-	printf("T: %.2f\t", T);
-	printf("P: %.2f\t", P);
-	printf("a: %.2f\n", a);
+	bmp180 = wiringPiI2CSetup(BMP180_ADDR);
+	while (bmp180) {
+		calibrate();	
+		readRawTemperature();
+		readRawPressure();
+		double T = getTemperature();
+		double P = getPressure();
+		double a = getAltitude();
+		printf("T: %.2f\t", T);
+		printf("P: %.2f\t", P);
+		printf("a: %.2f\n", a);
+	}
 }
 
