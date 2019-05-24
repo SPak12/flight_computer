c6adc1d3e9134c74d3471301796bc117c33581ad
SPak12 <seantwelve3@gmail.com>
2019-05-24 05:43:51 +0100

    Implemented multithreading, real-time

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <time.h>
 #include "altimeter.h"
 
-FILE *flight_data;
 int running; // Flag
 
 // Struct for storing data
@@ -21,43 +20,47 @@ void *telemetry(void *input) {
 }
 
 void *datalogging(void *input) {
-	flight_data = fopen("flight_data.txt", "w");
+	FILE *flight_data = fopen("flight_data.txt", "w");
+	if (flight_data == NULL) {
+		printf("\nError opening file!\n");
+		while(1); // Wait forever
+	}
 	fprintf(flight_data, "%.1f\t%.1f\n", data.time, data.altitude);
 	fclose(flight_data);
 	return 0;
 }
 
 // Returns monotonic time since start in seconds
-double getTime(uint64_t start_time) {
+uint64_t us_since_start(struct timespec start_time) {
 	struct timespec time;
-	clock_gettime(CLOCK_MONOTONIC, &time);
-	return (double) (time.tv_nsec - start_time) / (double) 10e9;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+	return (time.tv_sec - start_time.tv_sec) * 1e6 + (time.tv_nsec - start_time.tv_nsec) / 1e3;
 }
 
 // Updates data for telemetry and datalogging
-void updateData(double time, double temperature, double pressure, double altitude) {
-	data.time = time;
-	data.temperature = temperature;
-	data.pressure = pressure;
-	data.altitude = altitude;
+void updateData(double t, double T, double P, double a) {
+	data.time = t;
+	data.temperature = T;
+	data.pressure = P;
+	data.altitude = a;
 }
 
 int main(int argc, char *argv[]) {
 	running = 1;
 	wiringPiSetup();
 	int psens = wiringPiI2CSetup(BMP180_ADDR);
-	struct timespec start;
-	clock_gettime(CLOCK_MONOTONIC, &start);
-	uint64_t start_time = start.tv_nsec;
+	struct timespec start_time;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
 	while (running) {
-		double time = getTime(start_time);
+		double time = (double) us_since_start(start_time) / (double) 1e6;
 
 		// BMP180
 		calibrate(psens);
 		double T = getTemperature(psens);
 		double P = getPressure(psens);
 		double a = getAltitude(psens);
-		
+		printf("Time: %.1f\tTemperature: %.1f\tPressure: %.0f\tAltitude: %.1f\n", time, T, P, a);
+
 		updateData(time, T, P, a); // Update data for this time step
 
 		// Telemetry and Datalogging (Multithreading)
