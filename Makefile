26e98a6ddbbaaf79784d77509a1861ad3ed80700
SPak12 <seantwelve3@gmail.com>
2019-10-15 20:24:41 +0100

    Replaced main file

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CC = gcc
 CFLAGS = -I /usr/local/include -I ./BMP280_driver -L /usr/local/lib -lm -lwiringPi -lpthread
 DEPS = bmp280.h
-OBJ = test.o ./BMP280_driver/bmp280.o
+OBJ = main.o ./BMP280_driver/bmp280.o
 
 %.o: %.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-test: $(OBJ)
+main: $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS)
--- main.c
+++ main.c
@@ -1,70 +1,130 @@
-#include <stdio.h>
+#include "stdio.h"
 #include <stdlib.h>
 #include <pthread.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-#include "bmp180.h"
+#include "bmp280.h"
 
-int running; // Flag
+int running; // Running flag
+int psens;
 
-// Struct for storing data
-struct dataStruct {
-	double time;
-	double temperature;
-	double pressure;
-	double altitude;
-} data;
+void delay_ms(uint32_t period_ms);
+int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+void print_rslt(const char api_name[], int8_t rslt);
 
-void *telemetry(void *input) {
-	printf("Doing telemetry...\t");
-	return 0;
+int main(void) {
+    running = 1;
+    wiringPiSetup();
+    psens = wiringPiI2CSetup(BMP280_I2C_ADDR_SEC); // 0x77
+
+    int8_t rslt;
+    struct bmp280_dev bmp;
+    bmp.delay_ms = delay_ms;
+    bmp.dev_id = BMP280_I2C_ADDR_SEC;
+    bmp.intf = BMP280_I2C_INTF;
+    bmp.read = i2c_reg_read;
+    bmp.write = i2c_reg_write;
+
+    rslt = bmp280_init(&bmp);
+    print_rslt(" bmp280_init status", rslt);
+
+    return 0;
 }
 
-void *datalogging(void *input) {
-	FILE *flight_data = fopen("flight_data.txt", "a");
-	if (flight_data == NULL) {
-		printf("\nError opening file!\n");
-		while(1); // Wait forever
-	}
-	fprintf(flight_data, "%.2f\t%.2f\n", data.time, data.altitude);
-	fclose(flight_data);
-	return 0;
+/*!
+ *  @brief Function that creates a mandatory delay required in some of the APIs such as "bmg250_soft_reset",
+ *      "bmg250_set_foc", "bmg250_perform_self_test"  and so on.
+ *
+ *  @param[in] period_ms  : the required wait time in milliseconds.
+ *  @return void.
+ *
+ */
+void delay_ms(uint32_t period_ms)
+{
+    /* Implement the delay routine according to the target machine */
+    delay(period_ms);
 }
 
-// Updates data for telemetry and datalogging
-void updateData(double t, double T, double P, double a) {
-	data.time = t;
-	data.temperature = T;
-	data.pressure = P;
-	data.altitude = a;
+/*!
+ *  @brief Function for writing the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[in] reg_data : Pointer to the data buffer whose value is to be written.
+ *  @param[in] length   : No of bytes to write.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+
+    /* Implement the I2C write routine according to the target machine. */
+    for (int i = 0; i < length; i++) {
+        wiringPiI2CWriteReg8(psens, reg_addr + i, reg_data[i]);
+    }
+    return 0;
 }
 
-int main(int argc, char *argv[]) {
-	running = 1;
-	wiringPiSetup();
-	int psens = wiringPiI2CSetup(BMP180_ADDR);
-	unsigned int start_time = millis();
-	while (running) {
-		double time = (double) (millis() - start_time) / (double) 1e3;
+/*!
+ *  @brief Function for reading the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : Sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[out] reg_data    : Pointer to the data buffer to store the read data.
+ *  @param[in] length   : No of bytes to read.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
 
-		// BMP180
-		calibrate(psens);
-		double T = getTemperature(psens);
-		double P = getPressure(psens);
-		double a = getAltitude(psens);
-		printf("Time: %.2f\tAltitude: %.2f\n", time, a);
+    /* Implement the I2C read routine according to the target machine. */
+    for (int i = 0; i < length; i++) {
+        reg_data[i] = wiringPiI2CReadReg8(psens, reg_addr + i);
+    }
+    return 0;
+}
 
-		updateData(time, T, P, a); // Update data for this time step
-
-		// Telemetry and Datalogging (Multithreading)
-		pthread_t telemetry_t;
-		pthread_t datalogging_t;
-
-		pthread_create(&telemetry_t, NULL, telemetry, NULL);
-		pthread_create(&datalogging_t, NULL, datalogging, NULL);
-
-		pthread_join(telemetry_t, NULL);
-		pthread_join(datalogging_t, NULL);
-	}
-	return 0;
+/*!
+ *  @brief Prints the execution status of the APIs.
+ *
+ *  @param[in] api_name : name of the API whose execution status has to be printed.
+ *  @param[in] rslt     : error code returned by the API whose execution status has to be printed.
+ *
+ *  @return void.
+ */
+void print_rslt(const char api_name[], int8_t rslt)
+{
+    if (rslt != BMP280_OK)
+    {
+        printf("%s\t", api_name);
+        if (rslt == BMP280_E_NULL_PTR)
+        {
+            printf("Error [%d] : Null pointer error\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_COMM_FAIL)
+        {
+            printf("Error [%d] : Bus communication failed\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_IMPLAUS_TEMP)
+        {
+            printf("Error [%d] : Invalid Temperature\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_DEV_NOT_FOUND)
+        {
+            printf("Error [%d] : Device not found\r\n", rslt);
+        }
+        else
+        {
+            /* For more error codes refer "*_defs.h" */
+            printf("Error [%d] : Unknown error code\r\n", rslt);
+        }
+    }
 }
