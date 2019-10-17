653da908c60d2e2103781b3869f15ea177329bca
Sean Pak <spakman12@berkeley.edu>
2019-10-17 04:51:40 -0700

    Added header file for altimeter

--- altimeter.c
+++ altimeter.c
@@ -1,14 +1,6 @@
-#include "stdio.h"
-#include "bmp280.h"
+#include "altimeter.h"
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-
-void delay_ms(uint32_t period_ms);
-int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
-int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
-int8_t spi_reg_write(uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
-int8_t spi_reg_read(uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
-void print_rslt(const char api_name[], int8_t rslt);
 
 /*!
  *  @brief Function that creates a mandatory delay required in some of the APIs such as "bmg250_soft_reset",
